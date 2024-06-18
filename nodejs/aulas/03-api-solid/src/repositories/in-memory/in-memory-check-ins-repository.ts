b7133598658083dfb6ad208ea66e41b4eb6676ea
rafaelitajahy <rafaelitajahy@gmail.com>
2024-06-18 10:18:21 -0300

    feat: implementado casos  de uso - caso de uso de historico

--- nodejs/aulas/03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ nodejs/aulas/03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,9 +1,32 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { randomUUID } from 'crypto'
 import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20) // 1 - 1 * 20 = 0 / 20; 2 -1 * 20 = 20, 2 *  20 40
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+    if (!checkInOnSameDate) {
+      return null
+    }
+    return checkInOnSameDate
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
