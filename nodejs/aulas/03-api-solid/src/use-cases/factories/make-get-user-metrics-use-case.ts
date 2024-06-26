0d60811d7d2a734743732a77a1f78347a8ce14c7
rafaelitajahy <rafaelitajahy@gmail.com>
2024-06-26 10:39:56 -0300

    feat: implementa controler de perfil

--- nodejs/aulas/03-api-solid/src/use-cases/factories/make-get-user-metrics-use-case.ts
+++ nodejs/aulas/03-api-solid/src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { GetUserMetricsUseCase } from '../get-user-metrics'
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeRegisterUseCase() {
+export function makeGetUserMetricsUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new GetUserMetricsUseCase(checkInsRepository)
 
